50a5360a2a126eb7190b04dfd13ab86c9e60a461
Maxime Kuntz <maxime.kuntz75@gmail.com>
2026-09-06 20:39:44 +0200

    Update Storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,30 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import vue from 'eslint-plugin-vue';
 
-export default [
-  {
-    ignores: ['node_modules/', 'dist/', 'storybook-static/', '.git/', '.vscode/', 'coverage/'],
-  },
-  ...vue.configs['flat/recommended'],
-  {
-    files: ['src/**/*.{js,vue}', 'stories/**/*.js'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        console: 'readonly',
-        process: 'readonly',
-        document: 'readonly',
-        window: 'readonly',
-        setTimeout: 'readonly',
-        clearTimeout: 'readonly',
-      },
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      'vue/multi-word-component-names': 'off',
-      'vue/singleline-html-element-content-newline': 'off',
-      'vue/no-v-html': 'off',
-      'no-unused-vars': 'off',
+export default [{
+  ignores: ['node_modules/', 'dist/', 'storybook-static/', '.git/', '.vscode/', 'coverage/'],
+}, ...vue.configs['flat/recommended'], {
+  files: ['src/**/*.{js,vue}', 'stories/**/*.js'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: {
+      console: 'readonly',
+      process: 'readonly',
+      document: 'readonly',
+      window: 'readonly',
+      setTimeout: 'readonly',
+      clearTimeout: 'readonly',
     },
   },
-];
+  rules: {
+    ...js.configs.recommended.rules,
+    'vue/multi-word-component-names': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/no-v-html': 'off',
+    'no-unused-vars': 'off',
+  },
+}, ...storybook.configs["flat/recommended"]];
